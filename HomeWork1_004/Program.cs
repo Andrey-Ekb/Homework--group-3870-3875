e22830522bd98ff437c796b8527fabdc2692f3af
Андрей <122954595+Andrey-Ekb@users.noreply.github.com>
2023-02-02 13:22:24 +0500

    Parity check

---
+++ HomeWork1_004/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить, является ли число четным
+Console.Write("Ведите число - ");
+int number = int.Parse(Console.ReadLine());
+int ost=(number%2);
+if (ost>0)
+    Console.WriteLine("Введенное число нечетное" );
+else
+    Console.WriteLine("Введеное число является четным");
