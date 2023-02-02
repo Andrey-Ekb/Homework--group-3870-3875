7348c01604fefc2cc4feca6e315d6a03b2e1b74b
Андрей <122954595+Andrey-Ekb@users.noreply.github.com>
2023-02-02 18:03:07 +0500

    Last number

---
+++ Homework1_006/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трехзначного числа
+Console.Clear();
+Console.Write("Введите трехзначное число ");
+int Number=int.Parse(Console.ReadLine());
+string strNumber=Number.ToString();
+    char Number3=strNumber[strNumber.Length-1];
+    Console.WriteLine("последняя цифра - "+Number3);
