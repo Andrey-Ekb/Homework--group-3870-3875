133455317bd428c65563598825c2f694bea8365a
Андрей <122954595+Andrey-Ekb@users.noreply.github.com>
2023-02-02 17:27:22 +0500

    even numbers

---
+++ Homework1_005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать четные числа с 1 по N.
+Console.Write("Введите последнее число - ");
+int n= int.Parse(Console.ReadLine());
+int number = 2;
+while(number<=n)
+{
+    Console.WriteLine(number);
+    number=number+2;
+}
