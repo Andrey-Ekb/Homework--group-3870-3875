3bad4a59fed1e4a012ef83b33fb29d83ee34339b
Андрей <122954595+Andrey-Ekb@users.noreply.github.com>
2023-02-02 18:21:24 +0500

    Second digit

---
+++ Homework1_007/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трехзначного числа
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int Number=int.Parse(Console.ReadLine());
+string strNumber=Number.ToString();
+    char Number2=strNumber[strNumber.Length-2];
+    Console.WriteLine("вторая цифра равна - "+Number2);
+ 
