7a9731ca6e1feb87735346487e879bf3a68da6a1
Андрей <122954595+Andrey-Ekb@users.noreply.github.com>
2023-02-03 01:50:16 +0500

    multiple

---
+++ Homework1_009/Program.cs
@@ -0,0 +1,15 @@
+﻿// Удалить вторую цифру трехзначного числа
+Console.WriteLine("Введите трехзначное число - ");
+int Number = int.Parse(Console.ReadLine());
+
+int firsNum=Number/100;
+
+int secondNum=(Number-(firsNum*100))/10;
+
+int fiNum=(Number-(firsNum*100)-(secondNum*10));
+
+int rez= (firsNum*10)+fiNum;
+
+Console.WriteLine(rez);
+
+
---
+++ Homework1_010/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+Console.WriteLine("Ввети кратное число - ");
+int Number1=int.Parse(Console.ReadLine());
+Console.WriteLine("Ввети делитель числа - ");
+int Number2=int.Parse(Console.ReadLine());
+int ost=Number1 % Number2;
+if (ost>0)
+{ 
+    Console.WriteLine("число "+ Number1 + " неявляется кратным, при делителт "+ Number2+" т.к. остаток равен "+ost);
+}
+else
+{
+    Console.WriteLine("число "+ Number1 + " является кратным, при делителт "+ Number2+", остаток равен 0");
+}
