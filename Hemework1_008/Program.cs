007011e478c5eb8fa7d81091a3b478be7dfe6015
Андрей <122954595+Andrey-Ekb@users.noreply.github.com>
2023-02-03 15:42:11 +0500

    Maximum value

---
+++ Hemework1_008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число из отрезка(10,99). Показать наибольшую цифру числа
+
+int Number1=10;
+int Number2=99;
+int NumberMax=0;
+
+
+if (Number1 > Number2)
+{
+    NumberMax=Number1;
+}
+else 
+{
+    NumberMax=Number2;
+}
+
+Console.WriteLine("Наибольшая цифра - "+ NumberMax);
