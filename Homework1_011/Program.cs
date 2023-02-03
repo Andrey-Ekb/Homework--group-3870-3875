c9ab3fc0cdf23fab75a2c1f00bd124b82c8f06f4
Андрей <122954595+Andrey-Ekb@users.noreply.github.com>
2023-02-03 11:22:25 +0500

    Third digit check

---
+++ Homework1_011/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+
+Console.WriteLine("Введите число - ");
+int value=int.Parse(Console.ReadLine() ??"0");
+int Rez=value/100%10;
+
+if (Rez>0)
+    Console.WriteLine("Третья цифра равна - "+Rez);
+else
+    Console.WriteLine("В числе нет треьей цифры.");
